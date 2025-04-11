1beacb8e2f268d8c5e60ee2328e8698be6666db2
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-04-11 17:58:07 +0200

    fixed bug in booking

--- Modules/BookingModule/Http/Traits/BookingTrait.php
+++ Modules/BookingModule/Http/Traits/BookingTrait.php
@@ -56,7 +56,6 @@ trait BookingTrait
 
         $bookingIds = [];
         foreach ($cartData->pluck('sub_category_id')->unique() as $subCategory) {
-            dd($cartData->first()->coupon_id);
 
             $booking = new Booking();
 
@@ -105,7 +104,7 @@ trait BookingTrait
                 $booking->total_discount_amount = $cartData->sum('discount_amount');
                 $booking->total_campaign_discount_amount = $cartData->sum('campaign_discount');
                 $booking->total_coupon_discount_amount = $cartData->sum('coupon_discount');
-                $booking->coupon_id = $cartData->coupon_id;
+                $booking->coupon_id = $cartData->first()->coupon_code;
                 $booking->coupon_code = $cartData->first()->coupon_code;
                 $booking->service_schedule = date('Y-m-d H:i:s', strtotime($request['service_schedule'])) ?? now()->addHours(5);
                 $booking->service_address_id = $request['service_address_id'] ?? '';
