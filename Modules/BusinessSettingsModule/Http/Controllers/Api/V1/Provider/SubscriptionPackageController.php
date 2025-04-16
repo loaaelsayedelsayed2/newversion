2933514443e45d39e568719c497d5eb5024da80d
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-04-16 16:00:50 +0200

    transaction store payment_id

--- Modules/BusinessSettingsModule/Http/Controllers/Api/V1/Provider/SubscriptionPackageController.php
+++ Modules/BusinessSettingsModule/Http/Controllers/Api/V1/Provider/SubscriptionPackageController.php
@@ -327,7 +327,7 @@ class SubscriptionPackageController extends Controller
                 'payment_id' => $packageSubscriber->payment_id,
                 'created_at' => now(),
             ]);
-            $packageSubscriber->packageSubscriberLog->primary_transaction_id  = $transaction->id;
+            $packageSubscriber->logs->primary_transaction_id  = $transaction->id;
             $packageSubscriber->save();
             return response()->json(response_formatter(DEFAULT_200, $packageSubscriber), 200);
         } else {
