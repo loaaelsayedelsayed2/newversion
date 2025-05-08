ab67f4ebc9a0e6285c19e1fc1d97bd784b205c99
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-08 16:00:50 +0300

    reblace Zabbetny

--- Modules/BusinessSettingsModule/Resources/views/admin/partials/invoice.blade.php
+++ Modules/BusinessSettingsModule/Resources/views/admin/partials/invoice.blade.php
@@ -19,7 +19,7 @@
                     Av: 11, R: 09, Dhaka 1216, BD
                 </div>
                 <div>+8801100000001</div>
-                <div>demandium@demo.com</div>
+                <div>Zabbetny@demo.com</div>
             </div>
         </div>
 
@@ -91,5 +91,5 @@
             </div>
         </div>
     </div>
-    <div class="invoice-footer">All rights reserved By @DemandiumLtd 2024</div>
+    <div class="invoice-footer">All rights reserved By @ZabbetnyLtd 2024</div>
 </div>
