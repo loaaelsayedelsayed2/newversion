107a63f836f2714f80fe610307b6020b6c57de36
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-09 02:42:13 +0300

    show data provider in invoice3

--- Modules/ProviderManagement/Entities/Provider.php
+++ Modules/ProviderManagement/Entities/Provider.php
@@ -93,6 +93,11 @@ class Provider extends Model
         return $this->hasMany(Serviceman::class, 'provider_id');
     }
 
+    public function users()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
     public function reviews(): HasMany
     {
         return $this->hasMany(Review::class, 'provider_id', 'id');
