4e8e8cb5b8347b3578cb037c676971329317d9d1
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-09 16:41:04 +0300

    lat and log filter

--- Modules/ProviderManagement/Services/ProviderFilterService.php
+++ Modules/ProviderManagement/Services/ProviderFilterService.php
@@ -11,6 +11,21 @@ class ProviderFilterService
             if ($request->has('name')) {
                 $query->where('company_name', 'like', '%' . $request->input('name') . '%');
             }
+            if ($request->has('latitude') && $request->has('longitude')) {
+                $latitude = $request->input('latitude');
+                $longitude = $request->input('longitude');
+
+                $query->orderByRaw(
+                    "ST_Distance_Sphere(
+                        POINT(?, ?),
+                        POINT(
+                            JSON_UNQUOTE(JSON_EXTRACT(coordinates, '$.lng')),
+                            JSON_UNQUOTE(JSON_EXTRACT(coordinates, '$.lat'))
+                        )
+                    )",
+                    [$longitude, $latitude]
+                );
+            }
         };
     }
 }
