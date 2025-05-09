06c1ee1fb522a8f0c910107ce1dd339bd2047a8b
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-09 18:22:02 +0300

    filter by fav5

--- Modules/ProviderManagement/Http/Controllers/Api/V1/Customer/ProviderController.php
+++ Modules/ProviderManagement/Http/Controllers/Api/V1/Customer/ProviderController.php
@@ -245,7 +245,7 @@ class ProviderController extends Controller
     public function getProviderListBySubCategory(Request $request): JsonResponse
     {
         $filterService = app(ProviderFilterService::class);
-        $providers = $this->provider->with(['owner', 'customerFavorites' => function($query) use ($request) {
+        $providers = $this->provider->with(['owner', 'favorites' => function($query) use ($request) {
             $query->where('customer_user_id', $request->user()->id);
         }])
         ->where('zone_id', Config::get('zone_id'))
