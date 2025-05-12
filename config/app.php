acc14ad91bf1b047adc412302ac24ccb22e73f34
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-12 10:51:37 +0300

    edit in invoice for booking

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'custom_url' => 'https://zabbetny.com/',
+    'custom_url' => 'https://zabbetny.com',
 
     'asset_url' => env('ASSET_URL', null),
 
