62db51d370c7c04418a775c6ee301c9de1df94c6
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-19 16:43:20 +0300

    test is_paid5

--- app/Lib/Helpers.php
+++ app/Lib/Helpers.php
@@ -790,14 +790,13 @@ if (!function_exists('getPaymentGatewayImageFullPath')) {
 if (!function_exists('nextBookingEligibility')) {
     function nextBookingEligibility($providerId): bool
     {
+        dd($providerId);
         $now = \Carbon\Carbon::now()->subDay();
         $packageSubscriber = PackageSubscriber::where('provider_id', $providerId)->first();
         $packageSubscriberLogId = $packageSubscriber?->package_subscriber_log_id;
         $providerUserId = $packageSubscriber?->provider?->user_id;
         $isPaid = $packageSubscriber?->payment?->where('id', $packageSubscriber?->payment_id)->value('is_paid');
-        dd($packageSubscriber);
         $isPaidMoyasar = $packageSubscriber->where('payment_method','Moyasar')->get();
-        dd($isPaidMoyasar);
         if ($packageSubscriber && $packageSubscriber->payment_id != null) {
             if ($isPaid){
                 if ($packageSubscriber->is_canceled){
