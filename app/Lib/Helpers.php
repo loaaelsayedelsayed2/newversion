1441dd736bb9b7fcd024e3ca305511a190b6b49d
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-05-19 16:46:55 +0300

    test is_paid7

--- app/Lib/Helpers.php
+++ app/Lib/Helpers.php
@@ -795,7 +795,7 @@ if (!function_exists('nextBookingEligibility')) {
         $packageSubscriberLogId = $packageSubscriber?->package_subscriber_log_id;
         $providerUserId = $packageSubscriber?->provider?->user_id;
         $isPaid = $packageSubscriber?->payment?->where('id', $packageSubscriber?->payment_id)->value('is_paid');
-        $isPaidMoyasar = $packageSubscriber->where('payment_method','Moyasar')->exists();
+        $isPaidMoyasar = $packageSubscriber?->where('payment_method','Moyasar')->exists();
         if ($packageSubscriber && $packageSubscriber->payment_id != null) {
             if ($isPaid || $isPaidMoyasar) {
                 if ($packageSubscriber->is_canceled){
