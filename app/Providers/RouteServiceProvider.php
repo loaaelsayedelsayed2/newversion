ac4294fd0d3b27efc4ee333009769f8b421bdf67
loaaelsayedelsayed2 <loaaelsayed18@gmail.com>
2025-07-19 20:07:12 +0300

    fixed some conflites

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,9 +46,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/install.php'));
+
         });
     }
 
